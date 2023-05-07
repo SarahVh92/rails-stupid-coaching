6fca50e188ebaf45c7860474533a39ab4ee4d0cf
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-05-07 17:00:54 +0900

    Solved controller  bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,15 +3,17 @@ class QuestionsController < ApplicationController
 
   def answer
     # params is a hash in which we store all the key-value pairs
-    @question = params[:question]
+    @question = params[:ask]
 
     @answer =
-      if @question == 'I am going to work'
-        'Great!'
-      elsif @question.end_with?('?')
-        'Silly question, get dressed and go to work!'
-      else
-        "I don't care, get dressed and go to work!"
-      end
+    if @question.blank?
+      "🤭"
+    elsif @question == 'I am going to work'
+      'Great!'
+    elsif @question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
